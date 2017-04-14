5aa2e89bed2fbde41654feb8d61d44f1935281cb
johninvictus <invictusnitude@gmail.com>
2017-04-14 19:44:51 +0300

    testing CLI module

--- lib/concurrent_weather/cli.ex
+++ lib/concurrent_weather/cli.ex
@@ -21,17 +21,17 @@ defmodule ConcurrentWeather.CLI do
     parse = OptionParser.parse(argv, switches: [help: :boolean],
                                       alias: [h: :help])
 
-    case parse do
-      {[help: true], _, _} ->
-        :help
-
-      {_, {cities}, _} ->
-        {cities}
-
-      _ ->
-        :help
-    end
+    parse
+    |> parse
   end
+
+  def parse({[helps: true], _, _}), do: :help
+
+  def parse({_, [cities], _}) when is_list(cities) do
+    {cities}
+  end
+
+  def parse(_), do: :help
 
   def process(:help) do
     IO.puts "provide a list of cities."
--- test/concurrent_weather_test.exs
+++ test/concurrent_weather_test.exs
@@ -1,8 +1,17 @@
 defmodule ConcurrentWeatherTest do
   use ExUnit.Case
-  doctest ConcurrentWeather
+  import  ConcurrentWeather.CLI, only: [parse_argv: 1]
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "help will be returned if provided with -h or --help" do
+      assert parse_argv(["-h", "anything"]) == :help
+      assert parse_argv(["--help", "anything"]) == :help
+  end
+
+  test "nothing valid given" do
+    assert parse_argv(["value"]) == :help
+  end
+
+  test "provide a list of cities" do
+    assert parse_argv([["Nairobi", "Kisumu","Thika"]]) == {["Nairobi", "Kisumu","Thika"]}
   end
 end
